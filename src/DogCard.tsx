5e99800abd8d252ace5acda78693380ea560c73f
Sridhar Reddy <sridharrreddy@gmail.com>
2021-03-06 21:24:42 +1300

    wip: should fix performance

---
+++ src/DogCard.tsx
@@ -0,0 +1,48 @@
+import Avatar from '@material-ui/core/Avatar'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import {makeStyles} from '@material-ui/core/styles'
+import {Dog} from './DogTypes'
+
+const useStyles = makeStyles(theme => ({
+  dog: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+    textAlign: 'center',
+  },
+  dogName: {
+    paddingTop: theme.spacing(1),
+    fontSize: '1.1rem',
+  },
+  picture: {
+    width: 'initial',
+    height: 'initial',
+  },
+}))
+
+const DogCard = (dog: Dog) => {
+  const classes = useStyles()
+  return (
+    <Grid item xs={2} key={dog.Name}>
+      <Link href={dog.ProfileUrl} target="blank">
+        <Paper className={classes.dog}>
+          <div>
+            <Avatar
+              alt={dog.Name}
+              src={dog.ImgSrc}
+              className={classes.picture}
+              variant="rounded"
+            />
+          </div>
+          <Typography variant="subtitle1" className={classes.dogName}>
+            {dog.Name}
+          </Typography>
+        </Paper>
+      </Link>
+    </Grid>
+  )
+}
+
+export default DogCard
